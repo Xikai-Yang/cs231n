30520f1e7136485175294b644139b38aa5e1cc4f
Your Name <18810921006@163.com>
2020-09-10 09:27:59 +0800

    finish svm

--- assignment1_jupyter/assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1_jupyter/assignment1/cs231n/classifiers/linear_svm.py
@@ -36,10 +36,14 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:,j] += np.transpose(X[i])
+                dW[:,y[i]] -= np.transpose(X[i])
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
+    dW /= num_train
+    
 
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
